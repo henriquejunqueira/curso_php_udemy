321ee5b65b2b2e990e96f521807926b2eda4f050
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-04-05 10:54:29 -0300

    Iniciada estrutura do arquivo exericio.php e criado o arquivo de estilo exercicio.css

---
+++ exercicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="./assets/css/estilo.css" />
+    <title>Exercício</title>
+  </head>
+  <body>
+    <header class="cabecalho">
+      <h1>Curso PHP</h1>
+      <h2>Visualização do Exercício</h2>
+    </header>
+    <main class="principal">
+      <div class="conteudo">
+        
+      </div>
+    </main>
+    <footer class="rodape">COD3R & Henrique Junqueira © <?= date('Y')?></footer>
+  </body>
+</html>
