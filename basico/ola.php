6b452a45f45c18f6948a7d70ad37a7d2eb013712
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-04-12 11:44:05 -0300

    Olá PHP. Tipos de tags php. Quebra de linha. Phpinfo

---
+++ basico/ola.php
@@ -0,0 +1,18 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Exemplo A</h2>
+<?php 
+
+    echo 'Olá ';
+    // echo 'Olá <br>';
+    // echo "\n";
+    echo 'Mundo!'
+
+?>
+
+<!-- Tag PHP de impressão (ela já possui echo) -->
+<?= "Outra forma de me 'expressar'!" ?> 
+
+<?php
+
+phpinfo();
